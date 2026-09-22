39c96a89ea84e0b1895ec06a7a8dd3e4cafd11f0
RaulBaeza15 <RaulBaeza15@users.noreply.github.com>
2026-09-22 09:49:51 +0200

    Slovak sources

---
+++ i18n/glosses_sk.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Slovak glosses in catalog order (arise ... write), 145
+GLOSSES = [
+"vzniknúť","zobudiť sa","byť","porodiť","biť","stať sa","začať","ohnúť","staviť sa","zviazať",
+"hrýzť","krvácať","fúkať","zlomiť","chovať","priniesť","vysielať","stavať","horieť","prasknúť",
+"kúpiť","hodiť","chytiť","vybrať","držať sa","prísť","stáť","plaziť sa","rezať","rozdávať",
+"kopať","urobiť","kresliť","snívať","piť","šoférovať","bývať","jesť","padať","kŕmiť",
+"cítiť","bojovať","nájsť","sedieť","utiecť","švihnúť","lietať","zakázať","predpovedať","zabudnúť",
+"odpustiť","zamrznúť","dostať","dať","ísť","mleť","rásť","obesiť","mať","počuť",
+"skryť","udrieť","držať","zraniť","uchovať","kľakať","pliesť","vedieť","položiť","viesť",
+"oprieť sa","preskočiť","učiť sa","opustiť","požičať","nechať","ležať","zapáliť","stratiť","vyrobiť",
+"znamenať","stretnúť","zaplatiť","položiť","skončiť","čítať","zbaviť sa","jazdiť","zvoniť","vstať",
+"bežať","povedať","vidieť","hľadať","predať","poslať","nastaviť","šiť","triasť","vyliať",
+"svietiť","strieľať","ukázať","zmrštiť sa","zatvoriť","spievať","potopiť sa","sadnúť","spať","šmýkať sa",
+"voňať","hovoriť","ponáhľať sa","hláskovať","minúť","rozliať","točiť sa","rozštiepiť","pokaziť","šíriť",
+"vyskočiť","stáť","ukradnúť","nalepiť","pichnúť","smrdieť","trafiť","prisahať","zametať","opuchnúť",
+"plávať","hojdať sa","vziať","učiť","trhať","rozprávať","myslieť","hádzať","rozumieť","zobudiť",
+"nosiť","plakať","vyhrať","navíjať","písať",
+]
