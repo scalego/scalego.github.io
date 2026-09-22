0532d873414b5051e34e8c0ef2485e47bb9fe80a
Raúl Baeza Osuna <95641914+RaulBaeza15@users.noreply.github.com>
2026-09-23 00:24:40 +0200

    Sources: Irish (99)

---
+++ i18n/strings_ga.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+GA={'T_TITLE': 'Foghlaim Béarla · Briathra neamhrialta', 'T_TAB_PRACTICE': 'Cleachtadh', 'T_TAB_REVIEW': 'Athbhreithnigh an liosta', 'T_TAB_CREATE': 'Cruthaigh liosta', 'T_STATS_Q': 'Ceisteanna:', 'T_STATS_OK': '· Ceart:', 'T_STATS_BAD': '· Mícheart:', 'T_RESTART': 'Tosaigh arís', 'T_CHECK': 'Seiceáil', 'LANG': 'ga'}
