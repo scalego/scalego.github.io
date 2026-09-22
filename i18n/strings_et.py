9f3a55ab9069663fa7e99d97d7572b8bd90735ea
Raúl Baeza Osuna <95641914+RaulBaeza15@users.noreply.github.com>
2026-09-23 00:11:06 +0200

    Sources: Estonian (96)

---
+++ i18n/strings_et.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+ET={'T_TITLE': 'Õpi inglise keelt · Ebareeglipärased tegusõnad', 'T_TAB_PRACTICE': 'Harjuta', 'T_TAB_REVIEW': 'Vaata loend üle', 'T_TAB_CREATE': 'Loo loend', 'T_STATS_Q': 'Küsimused:', 'T_STATS_OK': '· Õige:', 'T_STATS_BAD': '· Vale:', 'T_RESTART': 'Alusta uuesti', 'T_CHECK': 'Kontrolli', 'T_TH_INF': 'Infinitiiv', 'T_TH_PP': 'Mineviku kesksõna', 'LANG': 'et'}
