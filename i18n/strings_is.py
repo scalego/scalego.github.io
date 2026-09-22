870da2d784fd191a5fbc42482d48e84297a9c039
Raúl Baeza Osuna <95641914+RaulBaeza15@users.noreply.github.com>
2026-09-23 00:27:32 +0200

    Sources: Icelandic (100)

---
+++ i18n/strings_is.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+IS={'T_TITLE': 'Lærðu ensku · Óreglulegar sagnir', 'T_TAB_PRACTICE': 'Æfa', 'T_TAB_REVIEW': 'Yfirfara listann', 'T_TAB_CREATE': 'Búa til lista', 'T_STATS_Q': 'Spurningar:', 'T_STATS_OK': '· Rétt:', 'T_STATS_BAD': '· Rangt:', 'T_RESTART': 'Byrja aftur', 'T_CHECK': 'Athuga', 'LANG': 'is'}
