af0d6754886b228be8bca0fa1870557439299917
RaulBaeza15 <RaulBaeza15@users.noreply.github.com>
2026-09-22 09:45:03 +0200

    Albanian sources

---
+++ i18n/glosses_sq.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Albanian glosses in catalog order (arise ... write), 145
+GLOSSES = [
+"lind","zgjohem","jam","mbart","rrah","bëhem","filloj","përkul","vë bast","lidh",
+"kafshoj","gjakos","fryj","thyej","mbarështoj","sjell","transmetoj","ndërtoj","djeg","plas",
+"blej","lëshoj","kap","zgjedh","ngjitem","vij","kushtoj","zvarritem","pres","ndaj",
+"gërmoj","bëj","vizatoj","ëndërroj","pi","ngas","banoj","ha","bie","ushqej",
+"ndihem","luftoj","gjej","përshtatem","ik","flak","fluturoj","ndaloj","parashikoj","harroj",
+"fal","ngrij","siguroj","jap","shkoj","bluaj","rrit","var","kam","dëgjoj",
+"fsheh","godit","mbaj","lëndoj","ruaj","gjunjëzohem","thur","di","shtroj","udhëheq",
+"mbështatem","kërcej","mësoj","largojem","jap hua","lë","shtrihem","ndez","humb","krijoj",
+"nënkuptoj","takoj","paguaj","vendos","hoq dorë","lexoj","shpëtoj","kalëroj","telefonoj","ngrihem",
+"vrapoj","them","shoh","kërkoj","shes","dërgoj","caktoj","qep","tund","derdh",
+"ndriçoj","qëlloj","tregoj","tkurrem","mbyll","këndoj","zhytem","ulem","fle","rrëshqas",
+"nuhas","flas","nxitoj","shqiptoj","shpenzoj","përmbyt","rrotulloj","çaj","prish","përhap",
+"hidhem","rri","vjedh","ngjit","thumbon","qelban","sulmoj","betohem","fshij","enjtem",
+"notoj","lëkundem","marr","jap mësim","gris","rrëfej","mendoj","hedh","kuptoj","zgjoj",
+"vesh","qaj","fitoj","mbështjell","shkruaj",
+]
