f8287214f2b9a73595117d140126fe57af6af60e
Raúl Baeza Osuna <95641914+RaulBaeza15@users.noreply.github.com>
2026-09-23 00:13:37 +0200

    Sources: Latvian (97)

---
+++ i18n/strings_lv.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+LV={'T_TITLE': 'Mācies angļu valodu · Neregulārie darbības vārdi', 'T_TAB_PRACTICE': 'Trenēties', 'T_TAB_REVIEW': 'Pārskatīt sarakstu', 'T_TAB_CREATE': 'Izveidot sarakstu', 'T_STATS_Q': 'Jautājumi:', 'T_STATS_OK': '· Pareizi:', 'T_STATS_BAD': '· Nepareizi:', 'T_RESTART': 'Sākt no jauna', 'T_CHECK': 'Pārbaudīt', 'LANG': 'lv'}
