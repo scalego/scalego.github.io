18756258753ca9c85123c998a1b4e99112a148c2
Raúl Baeza Osuna <95641914+RaulBaeza15@users.noreply.github.com>
2026-09-23 00:21:24 +0200

    Sources: Macedonian (98)

---
+++ i18n/strings_mk.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+MK={'T_TITLE': 'Учи англиски · Неправилни глаголи', 'T_TAB_PRACTICE': 'Вежбај', 'T_TAB_REVIEW': 'Прегледај ја листата', 'T_TAB_CREATE': 'Создај листа', 'T_STATS_Q': 'Прашања:', 'T_STATS_OK': '· Точно:', 'T_STATS_BAD': '· Погрешно:', 'T_RESTART': 'Почни повторно', 'T_CHECK': 'Провери', 'LANG': 'mk'}
